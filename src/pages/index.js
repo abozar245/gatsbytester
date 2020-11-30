d48136843453ef508a080c1616320642de5208d0
abozar245 <46285692+abozar245@users.noreply.github.com>
2020-11-30 18:00:03 +0330

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+      return (
+        <div style={{ color: `purple` }}>
+          <h1>Hello Gatsby!</h1>
+          <p>What a world.</p>
+          <img src="https://www.gatsbyjs.com/static/ae64a18bc63a9713d0a88f2570abe0dc/60b3a/04-add-image.png" alt="" width="500px" height="300px" />
+        </div>
+      )
 }
+
